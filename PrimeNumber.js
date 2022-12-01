9c2c05f2ca23d010cb31f68286cd414b3cbecb39
sudhansu nayak <nayaksd2001@gmail.com>
2022-12-01 09:35:08 +0530

    check complete

--- PrimeNumber.js
+++ PrimeNumber.js
@@ -2,12 +2,12 @@
 
 let num = 13;
 let count = 0;
-for (let i = 1; i >= num; i++) {
- 	if (i % num === 0) {
+ for(let i = 1; i<=num; i++) {
+ 	if (num % i == 0) {
 	  count++;
    }
 }
-  if (count < 3) {
+  if (count==2) {
      console.log("Yes");
    } else {
      console.log("No");
